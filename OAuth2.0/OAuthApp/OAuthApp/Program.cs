e151087c1b9f24d7dd43945ba33a424fb8900f68
bhanuchandra2010 <bhanuchandra2010@live.com>
2019-02-12 16:13:51 +0530

    Initial Basline

---
+++ OAuth2.0/OAuthApp/OAuthApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuthApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string applicationId = ConfigurationManager.AppSettings["appId"];
+            string clientsecret = ConfigurationManager.AppSettings["clientsecret"];
+        }
+    }
+}
